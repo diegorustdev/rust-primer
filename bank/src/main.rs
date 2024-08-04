a78706f4d10a6b7e6a956d28ad53296ce9ebed72
diego <diegorustdev@gmail.com>
2024-08-04 14:06:27 -0300

    adding inherent implementations

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -7,10 +7,26 @@ struct Account {
 
 }
 
+impl Account {
+    fn new(id: u32, holder: String) -> Self {
+        Account {
+            id,
+            holder,
+            balance: 0,
+        }
+    }
+}
+
 #[derive(Debug)]
 struct Bank {
     accounts: Vec<Account>,
 
+}
+
+impl Bank {
+    fn new() -> Self {
+        Bank { accounts: vec![] }
+    }
 }
 
 fn main() {
